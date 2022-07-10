c0b1d4ca69640c11fe71189f7c2d49edb8f7a0c8
Alex <belova.al.ig@gmail.com>
2022-07-10 22:39:01 +0300

    Program created

--- Task_01/Program.cs
+++ Task_01/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿Console.WriteLine("Insert the first number:");
+int numberA = Convert.ToInt32(Console.ReadLine( ));
+Console.WriteLine("Insert the second number:");
+int numberB = Convert.ToInt32(Console.ReadLine( ));
+double power = Math.Pow(numberB, 2);
+if (numberA == power)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
