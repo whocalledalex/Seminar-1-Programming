52ab461170a121a85df922118d2105171b5b22c1
Alex <belova.al.ig@gmail.com>
2022-07-10 22:16:01 +0300

    Made my program even better

--- Task_00/Program.cs
+++ Task_00/Program.cs
@@ -1,5 +1,7 @@
 ﻿Console.WriteLine("Insert your number:");
 int number = Convert.ToInt32(Console.ReadLine( ));
-int sqr = number * number;
-Console.WriteLine($"Square of number {number} = {sqr}");
+Console.WriteLine("Insert the power:");
+int power = Convert.ToInt32(Console.ReadLine( ));
+double result = Math.Pow(number, power);
+Console.WriteLine($"Power {power} of number {number} = {result}");
 Console.WriteLine("Thank you for using our program!");
