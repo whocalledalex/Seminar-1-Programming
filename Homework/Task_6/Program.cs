69c454c5669bc87a75ea455dd112cf92e5724f57
Alex <belova.al.ig@gmail.com>
2022-07-11 00:02:34 +0300

    Initial commit

--- Homework/Task_6/Program.cs
+++ Homework/Task_6/Program.cs
@@ -3,3 +3,10 @@
 // 4 -> да
 // -3 -> нет
 // 7 -> нет
+
+Console.WriteLine("Insert the number:");
+int number = Convert.ToInt32(Console.ReadLine( ));
+if (number % 2 == 0)
+{
+    Console.WriteLine("The number is even");
+}
