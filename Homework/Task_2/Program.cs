7348c583d4bc171349892b5da7922bcd80835254
Alex <belova.al.ig@gmail.com>
2022-07-11 00:03:40 +0300

    Last update

--- Homework/Task_2/Program.cs
+++ Homework/Task_2/Program.cs
@@ -1,4 +1,10 @@
-﻿Console.WriteLine("Insert the first number:");
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Insert the first number:");
 int numberA = Convert.ToInt32(Console.ReadLine( ));
 Console.WriteLine("Insert the second number:");
 int numberB = Convert.ToInt32(Console.ReadLine( ));
