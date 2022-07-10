d80cdd9c060b7c7bd2ccd1a5be8649ff5480338d
Alex <belova.al.ig@gmail.com>
2022-07-11 00:42:46 +0300

    Program is working

--- Homework/Task_8/Program.cs
+++ Homework/Task_8/Program.cs
@@ -5,3 +5,18 @@
 
 Console.WriteLine("Insert the number:");
 int N = Convert.ToInt32(Console.ReadLine( ));
+int index = 0;
+int number = 0;
+while (index < N)
+{
+    if (number % 2 == 0)
+    {
+        Console.Write($"{number}, ");
+    }
+    index = index + 1;
+    number = number + 1;
+}
+if (N % 2 == 0)
+{
+    Console.Write(N);
+}
