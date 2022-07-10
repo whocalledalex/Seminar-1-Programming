19a67a432821ab44bad5125c57854407f10a50f1
Alex <belova.al.ig@gmail.com>
2022-07-10 22:52:03 +0300

    Created a program with weekdays

---
+++ Task_02/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Insert the number of the weekday:");
+int number = Convert.ToInt32(Console.ReadLine( ));
+if (number == 1)
+{
+     Console.WriteLine("Monday");
+}
+else if (number == 2)
+{
+     Console.WriteLine("Tuesday");
+}
+else if (number == 3)
+{
+     Console.WriteLine("Wednesday");
+}
+else if (number == 4)
+{
+     Console.WriteLine("Thursday");
+}
+else if (number == 5)
+{
+     Console.WriteLine("Friday");
+}
+else if (number == 6)
+{
+     Console.WriteLine("Saturday");
+}
+else if (number == 7)
+{
+     Console.WriteLine("Sunday");
+}
+else
+{
+     Console.WriteLine("ERROR");
+}
