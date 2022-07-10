123a8dd642cfbb89f8e7d41b794d04fcb9c27362
Alex <belova.al.ig@gmail.com>
2022-07-10 23:49:48 +0300

    Program is working

--- Homework/Task_2/Program.cs
+++ Homework/Task_2/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿Console.WriteLine("Insert the first number:");
+int numberA = Convert.ToInt32(Console.ReadLine( ));
+Console.WriteLine("Insert the second number:");
+int numberB = Convert.ToInt32(Console.ReadLine( ));
+int max = numberA;
+if (max < numberB)
+{
+    max = numberB;
+}
+Console.WriteLine($"Maximum number = {max}");
